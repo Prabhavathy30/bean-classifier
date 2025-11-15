3598d7d93554d3560f2d077c9ac7a39775a3a786
Prabhavathy30 <prabhavathyganesan30@gmail.com>
2025-11-15 17:59:56 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "fb79332a-cc5f-4931-a5e5-cf7f7fa59d76",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
